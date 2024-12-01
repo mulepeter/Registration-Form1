fcd4dde37b1db806569262b645a2fdbee55db9cb
mulepeter <mulepetercr7@gmail.com>
2024-12-01 12:33:43 +0300

    Initial commit

---
+++ src/reg_form/Reg_form.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reg_form;
+
+/**
+ *
+ * @author ANTONY
+ */
+public class Reg_form {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
